d7df9df6c584a4d6bf2092b6227f72d1e3ab1093
Daniel Boye <danielboye888@gmail.com>
2024-08-04 12:10:04 +0200

    update help messages

--- src/cli.rs
+++ src/cli.rs
@@ -4,15 +4,15 @@ use clap::Parser;
 #[command(name = "sigcrack")]
 #[command(about = "A tool to find Solidity function signature collisions", long_about = None)]
 pub struct Cli {
-    #[arg(short, long)]
+    #[arg(short, long, help = "The target hash to find a collision for")]
     pub target_hash: String,
 
-    #[arg(short, long)]
+    #[arg(short, long, help = "Optional prefix for the function name")]
     pub prefix: Option<String>,
 
-    #[arg(short, long)]
+    #[arg(short, long, help = "Optional function parameter")]
     pub suffix: Option<String>,
 
-    #[arg(short, long)]
+    #[arg(short, long, help = "Length of the function name")]
     pub length: Option<usize>,
 }
